b15009e1036c69471aa11f21cd87a7d028465a23
lees569 <lees569@newschool.edu>
2016-03-16 19:52:25 -0400

    reupload

---
+++ week4/recursiveTreeObject/src/Tree.hpp
@@ -0,0 +1,29 @@
+//
+//  Tree.hpp
+//  recursiveTreeObject
+//
+//  Created by SHLEE on 2/24/16.
+//
+//
+
+#ifndef Tree_hpp
+#define Tree_hpp
+
+#include <stdio.h>
+#include "ofMain.h"
+
+#endif /* Tree_hpp */
+
+
+class Tree{
+
+public:
+    
+    float dotSize, angleOffSetA, angleOffSetB;
+    
+    void seed1( float dotSize, float anlge, float x, float y );
+    void seed2( float dotSize, float anlge, float x, float y );
+
+
+
+};
